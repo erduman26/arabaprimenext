4dade17759cf06aa6c1760a6eb76e095198ff318
erdem ceviz <erduman262@gmail.com>
2024-07-18 21:39:20 +0300

    one commit

--- src/app/urunler/page.tsx
+++ src/app/urunler/page.tsx
@@ -30,6 +30,87 @@ const Urunler = () => {
           </div>
         </div>
       </div>
+      <div className="relative border rounded-lg p-2 lg:p-3 shadow-lg ">
+        <div className="relative">
+          <img
+            className="w-full rounded-lg"
+            src="https://images.pexels.com/photos/7220271/pexels-photo-7220271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 1x, https://images.pexels.com/photos/7220271/pexels-photo-7220271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2 2x"
+            alt="Ürün Adı"
+          />
+          <span className="absolute top-2 right-2 bg-red-500 text-white text-xs font-bold py-1 px-1 rounded-lg">
+            %10
+          </span>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-sm sm:text-base md:text-lg lg:text-lg font-semibold text-gray-900">Würth Oto Yıkama Süngeri</h3>
+          <div className="flex items-center mt-2">
+            <span className="text-base lg:text-xl font-bold text-gray-800 mr-2">₺125.00</span>
+            <span className="text-sm font-bold text-gray-500 line-through">₺140.00</span>
+          </div>
+          <div className="flex items-center mt-2 space-x-1 text-white">
+            <button className="text-base bg-red-500 px-3 py-1 rounded-md w-full flex items-center justify-center">
+              Detay
+            </button>
+            <button className="flex items-center text-white bg-red-500 px-3 py-1 rounded-md">
+              <HiShoppingCart className="w-6 h-6 ml-auto" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="relative border rounded-lg p-2 lg:p-3 shadow-lg ">
+        <div className="relative">
+          <img
+            className="w-full rounded-lg"
+            src="https://images.pexels.com/photos/7220271/pexels-photo-7220271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 1x, https://images.pexels.com/photos/7220271/pexels-photo-7220271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2 2x"
+            alt="Ürün Adı"
+          />
+          <span className="absolute top-2 right-2 bg-red-500 text-white text-xs font-bold py-1 px-1 rounded-lg">
+            %10
+          </span>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-sm sm:text-base md:text-lg lg:text-lg font-semibold text-gray-900">Würth Oto Yıkama Süngeri</h3>
+          <div className="flex items-center mt-2">
+            <span className="text-base lg:text-xl font-bold text-gray-800 mr-2">₺125.00</span>
+            <span className="text-sm font-bold text-gray-500 line-through">₺140.00</span>
+          </div>
+          <div className="flex items-center mt-2 space-x-1 text-white">
+            <button className="text-base bg-red-500 px-3 py-1 rounded-md w-full flex items-center justify-center">
+              Detay
+            </button>
+            <button className="flex items-center text-white bg-red-500 px-3 py-1 rounded-md">
+              <HiShoppingCart className="w-6 h-6 ml-auto" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="relative border rounded-lg p-2 lg:p-3 shadow-lg ">
+        <div className="relative">
+          <img
+            className="w-full rounded-lg"
+            src="https://images.pexels.com/photos/7220271/pexels-photo-7220271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 1x, https://images.pexels.com/photos/7220271/pexels-photo-7220271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2 2x"
+            alt="Ürün Adı"
+          />
+          <span className="absolute top-2 right-2 bg-red-500 text-white text-xs font-bold py-1 px-1 rounded-lg">
+            %10
+          </span>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-sm sm:text-base md:text-lg lg:text-lg font-semibold text-gray-900">Würth Oto Yıkama Süngeri</h3>
+          <div className="flex items-center mt-2">
+            <span className="text-base lg:text-xl font-bold text-gray-800 mr-2">₺125.00</span>
+            <span className="text-sm font-bold text-gray-500 line-through">₺140.00</span>
+          </div>
+          <div className="flex items-center mt-2 space-x-1 text-white">
+            <button className="text-base bg-red-500 px-3 py-1 rounded-md w-full flex items-center justify-center">
+              Detay
+            </button>
+            <button className="flex items-center text-white bg-red-500 px-3 py-1 rounded-md">
+              <HiShoppingCart className="w-6 h-6 ml-auto" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

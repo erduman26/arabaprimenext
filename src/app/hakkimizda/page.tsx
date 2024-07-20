dd19ee2444a690ef19d7214801a51de5c7ad7b9d
erdem ceviz <erduman262@gmail.com>
2024-07-20 21:29:30 +0300

    three commit

--- src/app/hakkimizda/page.tsx
+++ src/app/hakkimizda/page.tsx
@@ -6,8 +6,8 @@ const Hakkimizda = () => {
     <section className="py-16">
         <div className="gap-16 items-center py-8 px-4 mx-auto 2xl:mx-20 lg:grid lg:grid-cols-2 lg:py-16">
         <div className="grid grid-cols-2 gap-4 mb-8 mt-0 lg:mt-8">
-            <Image className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
-            <Image className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
+            <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+            <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
           </div>
           <div className="font-light text-gray-500 sm:text-lg">
             <h2 className="mb-4 text-4xl tracking-tight font-bold text-gray-950">Hakkımızda!</h2>

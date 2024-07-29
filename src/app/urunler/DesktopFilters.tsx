008d58923ecf63b4e7d2b6796b2a7d79661d40b6
erdem ceviz <erduman262@gmail.com>
2024-07-29 22:20:32 +0300

    one update

--- src/app/urunler/DesktopFilters.tsx
+++ src/app/urunler/DesktopFilters.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import { HiMiniArrowsPointingOut, HiAdjustmentsHorizontal  } from 'react-icons/hi2';
+import { CiSearch } from "react-icons/ci";
 
 const DesktopFilters = () => {
   return (
     <div className="flex justify-center items-center bg-yellow-400 shadow-md 2xl:mx-24 px-4 rounded-lg py-4 font-semibold text-base mb-7">
-      <button className="flex items-center bg-gray-50 px-4 py-4 border-r-2 border-gray-200 rounded-l-md w-full justify-between">
-        <span>Marka</span>
-        <HiMiniArrowsPointingOut className='w-6 h-6'/>
-      </button>
+      <div className="relative w-full">
+        <input 
+          type="text" 
+          placeholder="Ara..." 
+          className="py-4 border-r-2 border-gray-200 rounded-l-lg w-full bg-gray-50 pl-10"
+        />
+        <CiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500" style={{ strokeWidth: 1 }}/>
+      </div>
       <button className="flex items-center bg-gray-50 px-4 py-4 border-r-2 border-gray-200 w-full justify-between">
-        <span>İndirimli Ürünler</span>
+        <span>Marka</span>
         <HiMiniArrowsPointingOut className='w-6 h-6'/>
       </button>
       <button className="flex items-center bg-gray-50 px-4 py-4 border-r-2 border-gray-200 w-full justify-between">
         <span>Kategoriler</span>
         <HiMiniArrowsPointingOut className='w-6 h-6'/>
       </button>
-      <button className="flex items-center bg-gray-50 px-4 py-4 w-full justify-between">
+      <button className="flex items-center bg-gray-50 px-4 py-4 border-r-2 border-gray-200 w-full justify-between">
         <span>Fiyat Aralığı</span>
         <HiMiniArrowsPointingOut className='w-6 h-6'/>
       </button>
-      <button className="flex items-center bg-red-600 px-4 py-3 text-white rounded-r-md w-full justify-between">
+      <button className="flex items-center bg-gray-50 px-4 py-4 w-full justify-between">
+        <span>Diğer</span>
+        <HiMiniArrowsPointingOut className='w-6 h-6'/>
+      </button>
+      <button className="flex items-center bg-red-600 px-4 gap-6 py-3 text-white rounded-r-md justify-between">
         <span>Filtrele</span>
         <HiAdjustmentsHorizontal className='w-8 h-8 text-white'/>
       </button>
--- src/app/urunler/page.tsx
+++ src/app/urunler/page.tsx
@@ -15,7 +15,31 @@ const Urunler = () => {
       <DesktopFilters />
       </div>
       </div>
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-4 lg:px-4 2xl:px-24 px-2">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 lg:gap-4 lg:px-4 2xl:px-24 px-2">
+      <div className="relative border rounded-lg p-2 lg:p-3 shadow-lg ">
+        <div className="relative">
+          <img
+            className="w-full rounded-lg"
+            src="https://images.pexels.com/photos/6621472/pexels-photo-6621472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 1x, https://images.pexels.com/photos/6621472/pexels-photo-6621472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2 2x"
+            alt="Ürün Adı"
+          />
+        </div>
+        <div className="mt-4">
+          <h3 className="text-sm sm:text-base md:text-lg lg:text-lg font-semibold text-gray-900 truncate">Würth Oto Yıkama Süngeri</h3>
+          <div className="flex items-center mt-2">
+            <span className="text-base lg:text-xl font-bold text-gray-800 mr-2">₺125.00</span>
+            <span className="text-sm font-bold text-gray-500 line-through">₺140.00</span>
+          </div>
+          <div className="flex items-center mt-2 space-x-1 text-white">
+            <button className="text-base font-semibold bg-red-600 px-3 py-1 rounded-md w-full flex items-center justify-center">
+              Detay
+            </button>
+            <button className="flex items-center text-white bg-red-600 px-3 py-1 rounded-md">
+              <HiOutlineShoppingBag className="w-6 h-6 ml-auto" style={{ strokeWidth: 1.9 }} />
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="relative border rounded-lg p-2 lg:p-3 shadow-lg ">
         <div className="relative">
           <img

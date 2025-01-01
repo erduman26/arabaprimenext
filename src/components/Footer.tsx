530d3650f604a64ebc5a0a3027f5ad181a5f9fe3
erdem ceviz <erduman262@gmail.com>
2025-01-01 23:18:16 +0300

    footerupdate

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,51 +4,88 @@ import Logo from "../img/logo.jpg";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center justify-center shadow-[0_-2px_2px_rgba(0,0,0,0.1)]">   
-        <footer className="p-10 bg-white text-gray-950 shadow-top text-center hidden lg:block">
-            <aside className='select-none text-sm'>
-                <Image
-                    className='relative sm:w-24 sm:h-24 w-20 h-20 rounded-3xl mx-auto'
-                    src={Logo}
+    <footer className="w-full bg-white shadow-[0_-2px_2px_rgba(0,0,0,0.1)]">
+        <div className="px-4 sm:px-6 2xl:px-24 pb-10 lg:pb-0">
+          {/*Grid*/}
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:flex lg:justify-between lg:gap-8 gap-12 pt-14 pb-8 2xl:px-2">
+            <div className="block">
+              <h4 className="text-base text-gray-800 font-bold mb-6">Arabaprime</h4>
+              <ul className="text-sm transition-all duration-500 font-semibold">
+                <li className="mb-4"><a href="javascript:;" className="text-gray-600 hover:text-red-600 transition-all duration-500">Anasayfa</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Hakkımızda</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Sepetim</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Üye Girişi</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Üye Ol</a></li>
+              </ul>
+            </div>
+            <div className="block">
+              <h4 className="text-base text-gray-800 font-bold mb-6">İlan Paketleri</h4>
+              <ul className="text-sm transition-all duration-500 font-semibold">
+                <li className="mb-4"><a href="javascript:;" className="text-gray-600 hover:text-red-600 transition-all duration-500">Ücretsiz İlan Paketi</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Destek İlan Paketi</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Uzman İlan Paketi</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Nitelikli İlan Paketi</a></li>
+              </ul>
+            </div>
+            <div className="block">
+              <h4 className="text-base text-gray-800 font-bold mb-6">Kurumsal</h4>
+              <ul className="text-sm transition-all duration-500 font-semibold">
+                <li className="mb-4"><a href="javascript:;" className="text-gray-600 hover:text-red-600 transition-all duration-500">İade / İptal Koşulları</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Mesafeli Satış Sözleşmesi</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Gizlilik ve Güvenlik</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Kişisel Verilerin Korunması</a></li>
+              </ul>
+            </div>
+            <div className="block">
+              <h4 className="text-base text-gray-800 font-bold mb-6">Yardım</h4>
+              <ul className="text-sm font-semibold">
+                <li className="mb-4"><a href="javascript:;" className="text-gray-600 hover:text-red-600 transition-all duration-500">Üyelik Sözleşmesi</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Bize Ulaşın</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Şifremi Unuttum</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Hesap Silme</a></li>
+                <li className="mb-4"><a href="javascript:;" className=" text-gray-600 hover:text-red-600 transition-all duration-500">Çerez Politikası</a></li>
+              </ul>
+            </div>
+            <div className="block">
+              <h4 className="text-base text-gray-800 font-bold mb-6">E-Ticaret Bilgi Sistemi</h4>
+              <a href="https://arabaprimenext.vercel.app/">
+              <img
+                    className='w-32 h-40'
+                    src="https://www.adenbilisim.com.tr/wp-content/uploads/2021/04/etbis.png"
                     alt="tozlupetlogo"
                     draggable="false"
                 />
-                <p className="font-bold mt-4">
-                    Arabaprime <br />2023 ten beri güvenilir hizmet.
-                </p>
-                <p className="mt-2">Copyright © 2023 - Tüm hakları saklıdır.</p>
-            </aside>
-            <nav className="mt-6">
-                <div className="flex justify-center space-x-4 text-gray-950">
-                    <a href='https://www.instagram.com/tozlupet' aria-label='Instagram'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 448 512" className='fill-current'>
-                            <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
-                        </svg>
-                    </a>
-                    <a href='https://www.facebook.com/tozlupet' aria-label='Facebook'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 448 512" className='fill-current'>
-                            <path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"/>
-                        </svg>
-                    </a>
-                    <a href='https://www.linkedin.com/in/erdem-ceviz' aria-label='Linkedin'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 448 512" className='fill-current'>
-                            <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/>
-                        </svg>
-                    </a>
-                    <a href='https://www.youtube.com' aria-label='Youtube'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
-                            <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
-                        </svg>
-                    </a>
-                    <a href='https://twitter.com' aria-label='Twitter'>
-                        <svg xmlns="http://www.w3.org/2000/svg" className='fill-current' width="24" height="24" viewBox="0 0 512 512">
-                            <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/>
-                        </svg>
+                </a>
+            </div>
+          </div>
+          {/*Grid*/}
+          <div className="pb-6 lg:py-5 border-t border-gray-400 2xl:px-2">
+            <div className="flex items-center justify-center flex-col lg:space-y-0 space-y-6 lg:justify-between lg:flex-row">
+                <div className='hidden lg:flex'>
+                    <a href="https://arabaprimenext.vercel.app/" className="flex justify-center items-center gap-3">
+                        <Image
+                            className='relative w-12 h-12 rounded-xl'
+                            src={Logo}
+                            alt="tozlupetlogo"
+                            draggable="false"
+                        />
+                        <h1 className="text-3xl font-bold text-gray-800">
+                            Araba<span className="text-red-600">prime</span>
+                        </h1>
                     </a>
                 </div>
-            </nav>
-        </footer>
-    </div>
+                <a href="https://arabaprimenext.vercel.app/" className='order-last lg:order-none text-sm font-semibold text-gray-600'>© 2024 Arabaprime Tüm Hakları Saklıdır.</a>
+              <div className="flex justify-center items-center mt-4 space-x-4 sm:mt-0">
+                <img src="linkedin.svg" className='w-9 h-9 p-1.5 rounded-lg flex justify-center items-center bg-red-600' alt="" />
+                <img src="youtube.svg" className='w-9 h-9 p-1.5 rounded-lg flex justify-center items-center bg-red-600' alt="" />
+                <img src="facebook.svg" className='w-9 h-9 p-1.5 rounded-lg flex justify-center items-center bg-red-600' alt="" />
+                <img src="x-twitter.svg" className='w-9 h-9 p-1.5 rounded-lg flex justify-center items-center bg-red-600' alt="" />
+                <img src="instagram.svg" className='w-9 h-9 p-1.5 rounded-lg flex justify-center items-center bg-red-600' alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
   );
 }
 
